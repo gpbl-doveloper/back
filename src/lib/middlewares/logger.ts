3a19ca92ef591eca19978d5eb09ea9e946ec29fc
haram8009 <haram8009@gmail.com>
2024-09-26 02:51:26 -0700

    feat: coloring http response status code

--- src/lib/middlewares/logger.ts
+++ src/lib/middlewares/logger.ts
@@ -2,6 +2,20 @@ import morgan, { StreamOptions } from "morgan";
 import logger from "jet-logger";
 import { Request } from "express";
 
+// ANSI 색상 코드 적용 함수
+const colorizeStatus = (status: number) => {
+  if (status >= 500) {
+    return `\x1b[31m${status}\x1b[0m`; // 빨간색 (500번대 에러)
+  } else if (status >= 400) {
+    return `\x1b[33m${status}\x1b[0m`; // 노란색 (400번대 에러)
+  } else if (status >= 300) {
+    return `\x1b[36m${status}\x1b[0m`; // 청록색 (300번대 리다이렉션)
+  } else if (status >= 200) {
+    return `\x1b[32m${status}\x1b[0m`; // 초록색 (200번대 성공)
+  }
+  return `\x1b[37m${status}\x1b[0m`; // 흰색 (기타)
+};
+
 // Morgan이 로그를 Jet-Logger로 보내도록 설정
 const stream: StreamOptions = {
   write: (message: string) => logger.info(message.trim()),
@@ -19,6 +33,11 @@ morgan.token("body", (req: Request) => {
 
 morgan.token("query", (req: Request) => {
   return JSON.stringify(req.query); // 요청 쿼리 데이터를 JSON 문자열로 변환
+});
+
+morgan.token("status", (req, res) => {
+  const status = res.statusCode;
+  return colorizeStatus(status); // 상태 코드에 ANSI 색상 코드 적용
 });
 
 const morganMW = morgan(morganFormat, { stream });
