6c002b1f706b01b53ac62e74b00520fadc930288
haram8009 <haram8009@gmail.com>
2024-10-04 01:25:34 -0700

    refactor: 코드 리팩토링

--- src/controllers/pictureController.ts
+++ src/controllers/pictureController.ts
@@ -5,34 +5,32 @@ import { File } from "@prisma/client";
 import { asyncWrapper } from "../middlewares/async";
 import prisma from "../lib/prisma";
 import { successResponse } from "../common/response";
+import { CustomError } from "../lib/error/customError";
+import ErrorCode from "../lib/error/errorCode";
 
-export const uploadFiles = async (req: Request, res: Response) => {
-  try {
-    const files = req.files as Express.Multer.File[]; // 여러 파일 배열
+export const uploadFiles = asyncWrapper(async (req: Request, res: Response) => {
+  const files = req.files as Express.Multer.File[]; // 여러 파일 배열
 
-    if (!files || files.length === 0) {
-      return res.status(400).json({ error: "No files uploaded" });
-    }
-
-    const uploadResults: File[] = [];
-
-    // 각 파일을 스토리지 서비스에 업로드
-    for (const file of files) {
-      const filePath = file.path;
-      const destination = `uploads/${file.filename}`;
-      const newFile = await storageService.uploadFile(filePath, destination);
-
-      uploadResults.push(newFile);
-
-      // 임시 파일 삭제
-      fs.unlinkSync(filePath);
-    }
-
-    res.json({ files: uploadResults });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
+  if (!files || files.length === 0) {
+    throw new CustomError(ErrorCode.NO_FILES_UPLOADED);
   }
-};
+
+  const uploadResults: File[] = [];
+
+  // 각 파일을 스토리지 서비스에 업로드
+  for (const file of files) {
+    const filePath = file.path;
+    const destination = `uploads/${file.filename}`;
+    const newFile = await storageService.uploadFile(filePath, destination);
+
+    uploadResults.push(newFile);
+
+    // 임시 파일 삭제
+    fs.unlinkSync(filePath);
+  }
+
+  successResponse(res, { files: uploadResults });
+});
 
 export const getFiles = asyncWrapper(async (req: Request, res: Response) => {
   const todayMidnight = new Date();
