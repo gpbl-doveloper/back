52a6b80f131534c99eaa83615cd6d2b349471955
haram8009 <haram8009@gmail.com>
2024-11-14 04:23:34 -0800

    feat: add reservation routes

--- src/routes/diaryRoutes.ts
+++ src/routes/diaryRoutes.ts
@@ -6,23 +6,33 @@ import {
 } from "../controllers/diaryController";
 import { upload } from "../middlewares/multerConfig";
 import paths from "../common/paths";
+import auth from "../middlewares/auth";
+import loginUser from "../middlewares/loginUser";
 
 const router = express.Router();
 
 /**
  * 다이어리 목록 라우트
  */
-router.get(paths.diary.get, getDiary);
+router.get(paths.diary.get, auth, loginUser, getDiary);
 
 /**
  * 다이어리 정보 라우트
  */
-router.get(paths.diary.info, getDiaryInfo);
+router.get(paths.diary.info, auth, loginUser, getDiaryInfo);
 
 /**
- * 다이어리 업로드 라우트
+ * 다이어리 Photo 업로드 라우트
  * //FIXME 파일업로드 없애기
  */
-router.post(paths.diary.add, upload.array("files", 20), addDiary);
+router.post(paths.diary.addPhoto, upload.array("files", 20), addDiary);
+
+/**
+ * 다이어리 Note 업로드 라우트
+ */
+router.post(paths.diary.addNote, upload.array("files", 20), addDiary);
+
+router.post(paths.diary.sendPhoto, addDiary);
+router.post(paths.diary.sendNote, addDiary);
 
 export default router;
