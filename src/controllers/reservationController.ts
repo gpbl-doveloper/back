3d97f3a0df83a109b697c3a452369d5ea7c76a1a
haram8009 <haram8009@gmail.com>
2024-11-28 17:00:11 -0800

    feat: reservation apis [TA-110]

--- src/controllers/reservationController.ts
+++ src/controllers/reservationController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import prisma from "../lib/prisma";
 import { asyncWrapper } from "../middlewares/async";
 import { successResponse } from "../common/response";
+import { CustomError } from "../lib/error/customError";
+import ErrorCode from "../lib/error/errorCode";
 
 export const createReservation = asyncWrapper(
   async (req: Request, res: Response) => {
@@ -32,5 +34,103 @@ export const createReservation = asyncWrapper(
     });
 
     successResponse(res, { reservation }, "Reservation created successfully");
+  }
+);
+
+export const getOwnerReservations = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const ownerId = req.loginUser?.id;
+
+    // TODO: user role validation
+    if (!ownerId) {
+      throw Error("Owner ID is required");
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: {
+        dog: {
+          ownerId,
+        },
+      },
+    });
+
+    successResponse(
+      res,
+      { reservations },
+      "Reservations retrieved successfully"
+    );
+  }
+);
+
+export const getCenterReservations = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const centerId = req.loginUser?.centerId;
+
+    // TODO: user role validation
+    if (!centerId) {
+      throw new Error("Center ID is required");
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: {
+        centerId,
+      },
+    });
+
+    successResponse(
+      res,
+      { reservations },
+      "Reservation requests retrieved successfully"
+    );
+  }
+);
+
+export const acceptReservation = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const reservationId = Number(req.params.id);
+
+    const reservation = await prisma.reservation.findUniqueOrThrow({
+      where: { id: reservationId },
+    });
+
+    if (reservation.status !== "PENDDING") {
+      throw new Error("Reservation already processed");
+    }
+
+    const updatedReservation = await prisma.reservation.update({
+      where: { id: reservationId },
+      data: { status: "ACCEPTED" },
+    });
+
+    successResponse(
+      res,
+      { reservation: updatedReservation },
+      "Reservation accepted successfully"
+    );
+  }
+);
+
+export const declineReservation = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const reservationId = Number(req.params.id);
+
+    const reservation = await prisma.reservation.findUniqueOrThrow({
+      where: { id: reservationId },
+    });
+
+    if (reservation.status !== "PENDDING") {
+      throw Error("Reservation already processed");
+    }
+
+    const updatedReservation = await prisma.reservation.update({
+      where: { id: reservationId },
+      data: { status: "DECLINED" },
+    });
+
+    successResponse(
+      res,
+      { reservation: updatedReservation },
+      "Reservation declined successfully"
+    );
   }
 );
